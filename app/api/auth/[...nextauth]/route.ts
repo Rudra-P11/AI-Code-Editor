26334a872234b87dc066765b4955d4d0548c4f9d
Rudra P <pandyarudra2@gmail.com>
2025-12-12 19:06:10 +0530

    Implement Auth services with Google & Github

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,2 +1,2 @@
-import { handlers } from "@/auth" // Referring to the auth.ts we just created
+import { handlers } from "@/auth" // Referring to the auth.ts we created in the root directory
 export const { GET, POST } = handlers
